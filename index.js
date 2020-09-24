9862b423013355ec4de2f3a4ad952589dd62dc1d
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-09-24 12:44:46 +0900

    Rewrite with new code convention

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
-WebJSFeed = (function() {
-    return {
-        _web_loaded: false,
-        _contexts: {}
+var module = (function() {
+    var _web_loaded = false, _contexts = {}
+
+    function _get_context(name) {
+        if (!_contexts[name]) {
+            _contexts[name] = {};
+        }
+    
+        return _contexts[name];
+    }
+    
+    return {  
+        feed: function(name, handler) {
+            var context = _get_context(name);
+        
+            if (_web_loaded) {
+                handler(context["next-token"]);
+            } else {
+                context["handler"] = handler;
+            }
+        },
+        
+        reset: function() {
+            _web_loaded = false, _contexts = {}
+        },
+        
+        on_feed_done: function(name, next_token) {
+            var context = _get_context(name);
+        
+            if (context["handler"]) {
+                delete context["handler"];
+            }
+        
+            context["next-token"] = next_token;
+        },
+        
+        on_web_loaded: function() {
+            _web_loaded = true;
+        
+            Object.keys(_contexts).forEach(function(name) {
+                var context = _get_context(name);
+                
+                if (context["handler"]) {
+                    context["handler"](context["next-token"]);
+                }
+            });
+        },
+        
+        is_web_loaded: function() {
+            return _web_loaded;
+        },    
     }
 })();
 
-WebJSFeed.feed = function(name, handler) {
-    var context = this._get_context(name);
-
-    if (this._web_loaded) {
-        handler(context["next-token"]);
-    } else {
-        context["handler"] = handler;
-    }
-}
-
-WebJSFeed.reset = function() {
-    this._web_loaded = false
-    this._contexts = {}
-}
-
-WebJSFeed.on_feed_done = function(name, next_token) {
-    var context = this._get_context(name);
-
-    if (context["handler"]) {
-        delete context["handler"];
-    }
-
-    context["next-token"] = next_token;
-}
-
-WebJSFeed.on_web_loaded = function() {
-    var self = this;
-
-    this._web_loaded = true;
-
-    Object.keys(this._contexts).forEach(function(name) {
-        var context = self._get_context(name);
-        
-        if (context["handler"]) {
-            context["handler"](context["next-token"]);
-        }
-    });
-}
-
-WebJSFeed.is_web_loaded = function() {
-    return WebJSFeed._web_loaded;
-}
-
-WebJSFeed._get_context = function(name) {
-    if (!this._contexts[name]) {
-        this._contexts[name] = {};
-    }
-
-    return this._contexts[name];
-}
-
-__MODULE__ = WebJSFeed;
+__MODULE__ = module;
